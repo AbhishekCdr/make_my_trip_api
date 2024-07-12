19e81cf486d1b1639f92e12e59b14da25fc95802
Abhishek Kumar <abhikr.ak.2001@gmail.com>
2024-07-12 23:57:49 +0530

    data route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,36 +64,8 @@ export const getListing = async (req, res, next) => {
 
 export const getListings = async (req, res, next) => {
   try {
-    const limit = parseInt(req.query.limit) || 9;
-    const startIndex = parseInt(req.query.startIndex) || 0;
-    let offer = req.query.offer;
-
-    if (offer === undefined || offer === "false") {
-      offer = { $in: [false, true] };
-    }
-
-    let parking = req.query.parking;
-
-    if (parking === undefined || parking === "false") {
-      parking = { $in: [false, true] };
-    }
-
-    const searchTerm = req.query.searchTerm || "";
-
-    const sort = req.query.sort || "createdAt";
-
-    const order = req.query.order || "desc";
-
-    const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: "i" },
-      offer,
-      parking,
-    })
-      .sort({ [sort]: order })
-      .limit(limit)
-      .skip(startIndex);
-
-    return res.status(200).json(listings);
+    const data = await Listing.find();
+    res.json(data);
   } catch (error) {
     next(error);
   }
