fec2ef901a11054f1e0c2720d2d257ece780bf5c
Abhishek Kumar <abhikr.ak.2001@gmail.com>
2024-07-14 20:59:57 +0530

    toekn

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -3,8 +3,11 @@ import { errorHandler } from "./error.js";
 
 export const VerifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
+  console.log(token);
 
-  if (!token) return next(errorHandler(401, "Unauthorized"));
+  if (!token) {
+    return next(errorHandler(401, "Unauthorized"));
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
